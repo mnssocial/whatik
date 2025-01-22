222b0997ab263d4533c9ce743f7d97bc1a287efa
Lucas Vasconcelos <lucas@meunegociosocial.com>
2025-01-22 20:05:03 -0300

    ww

--- frontend/server.js
+++ frontend/server.js
@@ -1,12 +1,10 @@
-
+//simple express server to run frontend production build;
 const express = require("express");
 const path = require("path");
 const app = express();
-require('dotenv').config();
-
-app.use(express.static(path.join(__dirname, "whaticketplus")));
-app.get("/*", function (_req, res) {
-	res.sendFile(path.join(__dirname, "whaticketplus", "index.html"));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+app.listen(3000);
 
-app.listen(process.env.SERVER_PORT || 3333);
